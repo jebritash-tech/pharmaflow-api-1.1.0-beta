506601d0d55ad5888cf0cd8a1e486fff8da9ddfa
Jebril_Hassan <jebritash@gmail.com>
2026-06-12 09:11:34 +0200

    Fix Faker Class Error

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class SupplierSeeder extends Seeder
 {
     public function run(): void
