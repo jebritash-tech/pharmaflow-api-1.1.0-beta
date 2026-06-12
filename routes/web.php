bfdff84018deacaa6fdebea1ca3d43c9a3d6326d
Jebril_Hassan <jebritash@gmail.com>
2026-06-12 08:58:14 +0200

    Add Setup Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/setup-system', function () {
+
+    Artisan::call('migrate', [
+        '--force' => true
+    ]);
+
+    Artisan::call('db:seed', [
+        '--force' => true
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'System initialized successfully'
+    ]);
 });
